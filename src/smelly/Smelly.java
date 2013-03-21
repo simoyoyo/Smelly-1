ebd55435b15aa2679467d4250007dc16ebd0fa7e
Eddy <bravogit@gmail.com>
2013-03-21 19:18:01 +0100

    Versione 1.0.0

--- src/smelly/Smelly.java
+++ src/smelly/Smelly.java
@@ -1,19 +1,38 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package smelly;
 
 /**
- *
- * @author Eddy
+ * Nell'orribile programma seguente dovrete operare come segue
+ * 1) Creare opportuni test Junit
+ * 2) Rifattorizzare (modificando il main di conseguenza e indicando che "puzza"
+ * avete rilevato e che metodo avete scelto per risolverlo)
+ * 3) Aggiungere gli opportuni Javadoc se non banali
+ * @author piffy
  */
 public class Smelly {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Solido c = new Solido();
+        c.H=2.0f;
+        c.r=7.f;
+        c.type=1;
+        c.calcola();
+        System.out.println("Area di superficie:"+c.A);
+        System.out.println("Volume: "+c.V);
+        Solido d = new Solido();
+        d.r=5.0f;
+        d.H=2.0f;
+        d.type=1;
+        d.calcola();
+        Solido e = new Solido();
+        e.r=5.3f;
+        e.type=2;
+        e.calcola();
+        Solido Birillo[] = new Solido[10];
+        Birillo[1]=c;Birillo[2]=d;Birillo[3]=e;
+        if (Birillo[1].V>=Birillo[2].V && Birillo[1].V>=Birillo[3].V) 
+            System.out.println("Il solido con volume maggiore è al posto 1");
+        else if (Birillo[2].V>=Birillo[1].V && Birillo[2].V>=Birillo[3].V) 
+            System.out.println("Il solido con volume maggiore è al posto 2");
+        else if (Birillo[3].V>=Birillo[1].V && Birillo[3].V>=Birillo[2].V) 
+            System.out.println("Il solido con volume maggiore è al posto 3");
     }
 }
