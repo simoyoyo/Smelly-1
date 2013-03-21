3975699bb8b15d090b57252ee05003643f45e2d0
Eddy <bravogit@gmail.com>
2013-03-21 19:13:36 +0100

    Creato il progetto Java.

---
+++ Smelly/src/smelly/Smelly.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smelly;
+
+/**
+ *
+ * @author Eddy
+ */
+public class Smelly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
